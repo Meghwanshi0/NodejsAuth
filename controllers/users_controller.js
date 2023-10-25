9249903847a1a5230973c09e30c7cfb6e7787ced
Hemant Kumar Meghwal <hemantmeghwanshi01@gmail.com>
2023-10-26 01:53:45 +0530

    File Uploads

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 const User = require('../models/users');
 const customMware = require('../config/middleware');
+const fs= require('fs');
+const path = require('path');
 
 module.exports.profile = function(req, res){
     User.findById(req.params.id)
@@ -15,20 +17,20 @@ module.exports.profile = function(req, res){
         });
 }
 
-module.exports.update = function(req, res){
-    if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body)
-            .then(user => {
-                return res.redirect('back');
-            })
-            .catch(err => {
-                console.error('Error updating user:', err);
-                return res.redirect('back');
-            });
-    } else {
-        return res.status(401).send('Unauthorized');
-    }
-}
+// module.exports.update = function(req, res){
+//     if(req.user.id == req.params.id){
+//         User.findByIdAndUpdate(req.params.id, req.body)
+//             .then(user => {
+//                 return res.redirect('back');
+//             })
+//             .catch(err => {
+//                 console.error('Error updating user:', err);
+//                 return res.redirect('back');
+//             });
+//     } else {
+//         return res.status(401).send('Unauthorized');
+//     }
+// }
   /*if (req.cookies.user_id){
         User.findById(req.cookies.user_id)
             .then(user => {
@@ -49,6 +51,38 @@ module.exports.update = function(req, res){
         return res.redirect('/users/sign-in');
     }
     */
+module.exports.update = async function(req, res){
+    if(req.user.id == req.params.id){
+
+        try{
+            let user=await User.findById(req.params.id);
+            User.uploadedAvatar(req,res, function(err){
+                if(err){
+                    console.log('****Multer Error:', err)
+                }   
+                user.name=req.body.name;
+                user.email=req.body.email;
+
+                if(req.file){
+                        // if(user.avatar){
+                        //     fs.unlinkSync(path.join(__dirname,'..',user.avatar));
+                        // }
+
+                    //this is saving the path of uploaded file into the avatar field in the user
+                    user.avatar=User.avatarPath + '/' +req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            }
+            );
+        }
+        catch(err){
+            req.flash('error', err);
+            return res.redirect('back');
+        }
+
+    }
+}
 
 
 module.exports.signIn= function(req, res){
